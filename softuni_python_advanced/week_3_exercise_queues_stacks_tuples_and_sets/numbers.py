26cb1913d18826e2e28f18772af7891f6c64c48c
Kamend1 <142220912+Kamend1@users.noreply.github.com>
2024-01-15 16:20:54 +0200

    I have now started Advanced and OOP in January 2024. New exercise problems solved

--- softuni_python_advanced/week_3_exercise_queues_stacks_tuples_and_sets/numbers.py
+++ softuni_python_advanced/week_3_exercise_queues_stacks_tuples_and_sets/numbers.py
@@ -1,30 +1,27 @@
-first_sequence = set(num for num in input().split())
-second_sequence = set(num for num in input().split())
+first_sequence = set(int(num) for num in input().split())
+second_sequence = set(int(num) for num in input().split())
 counter = int(input())
 
 for _ in range(counter):
     command = input().split()
-    first_command = command[0]
-    if first_command == "Add":
-        second_command = command[1]
-        if second_command == "First":
-            for num in command[2:]:
-                first_sequence.add(num)
-        elif second_command == "Second":
-            for num in command[2:]:
-                second_sequence.add(num)
-    elif first_command == "Remove":
-        second_command = command[1]
-        if second_command == "First":
-            for num in command[2:]:
-                if num in first_sequence:
-                    first_sequence.remove(num)
-        elif second_command == "Second":
-            for num in command[2:]:
-                if num in second_sequence:
-                    second_sequence.remove(num)
-    elif first_command == "Check":
+    if command[0] == "Add":
+        if command[1] == "First":
+            for num in command[2:len(command)]:
+                first_sequence.add(int(num))
+        elif command[1] == "Second":
+            for num in command[2:len(command)]:
+                second_sequence.add(int(num))
+    elif command[0] == "Remove":
+        if command[1] == "First":
+            for num in command[2:len(command)]:
+                if int(num) in first_sequence:
+                    first_sequence.remove(int(num))
+        elif command[1] == "Second":
+            for num in command[2:len(command)]:
+                if int(num) in second_sequence:
+                    second_sequence.remove(int(num))
+    else:
         print(second_sequence.issubset(first_sequence) or first_sequence.issubset(second_sequence))
 
-print(', '.join(sorted(first_sequence)))
-print(', '.join(sorted(second_sequence)))
+print(*sorted(first_sequence), sep=", ")
+print(*sorted(second_sequence), sep=", ")
