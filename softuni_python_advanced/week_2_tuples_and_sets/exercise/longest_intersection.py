b2ef52c47c449661bb8c86e4f77afc58a8435285
Kamend1 <142220912+Kamend1@users.noreply.github.com>
2024-01-15 19:36:44 +0200

    I have now started Advanced and OOP in January 2024. New exercise problems solved

--- softuni_python_advanced/week_2_tuples_and_sets/exercise/longest_intersection.py
+++ softuni_python_advanced/week_2_tuples_and_sets/exercise/longest_intersection.py
@@ -7,15 +7,13 @@ for _ in range(counter):
     raw_data.append(input())
 
 for data in raw_data:
-    set_1 = set()
-    set_2 = set()
     first_start, mid_range, second_end = data.split(',')
     first_end, second_start = mid_range.split('-')
-    for i in range(int(first_start), int(first_end) + 1):
-        set_1.add(i)
-    for j in range(int(second_start), int(second_end) + 1):
-        set_2.add(j)
+
+    set_1 = set(range(int(first_start), int(first_end) + 1))
+    set_2 = set(range(int(second_start), int(second_end) + 1))
     current_intersection = set_1.intersection(set_2)
+
     if len(current_intersection) > len(longest_intersection):
         max_length = len(current_intersection)
         longest_intersection = list(current_intersection)
