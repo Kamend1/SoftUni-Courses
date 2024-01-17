4a1dbd7d968a8faf3a769368b5b16201d21c3eb2
Kamend1 <142220912+Kamend1@users.noreply.github.com>
2024-01-17 16:12:36 +0200

    I have now started Advanced and OOP in January 2024. New exercise problems solved

--- softuni_python_advanced/week_3_exercise_queues_stacks_tuples_and_sets/santas_present_factory.py
+++ softuni_python_advanced/week_3_exercise_queues_stacks_tuples_and_sets/santas_present_factory.py
@@ -10,36 +10,31 @@ presents_magic_needed_dict = {
 materials = deque(int(x) for x in input().split())
 magic = deque(int(x) for x in input().split())
 
-crafted_toys = {}
+crafted_toys = []
 presents_crafted = False
 
 while materials and magic:
-    current_material = materials.pop()
-    current_magic = magic.popleft()
+    current_material = materials.pop() if magic[0] or not materials[-1] else 0
+    current_magic = magic.popleft() if current_material or not magic[0] else 0
+
+    if not current_magic:
+        continue
+
     product = current_material * current_magic
     if product in presents_magic_needed_dict.keys():
-        crafted_toy = presents_magic_needed_dict.get(product)
-        if crafted_toy not in crafted_toys:
-            crafted_toys[crafted_toy] = 0
-        crafted_toys[crafted_toy] += 1
+        crafted_toys.append(presents_magic_needed_dict.get(product))
     elif product > 0:
         current_material += 15
         materials.append(current_material)
     elif product < 0:
         current_material += current_magic
         materials.append(current_material)
-    elif product == 0:
-        if current_material != 0:
-            materials.append(current_material)
-        if current_magic != 0:
-            magic.appendleft(current_magic)
 
 if 'Doll' in crafted_toys and 'Wooden train' in crafted_toys:
     presents_crafted = True
 
 if 'Bicycle' in crafted_toys and 'Teddy bear' in crafted_toys:
     presents_crafted = True
-
 
 if presents_crafted:
     print(f"The presents are crafted! Merry Christmas!")
@@ -53,5 +48,4 @@ if materials:
 if magic:
     print_str = ', '.join(map(str, magic))
     print(f"Magic left: {print_str}")
-for toy, count in sorted(crafted_toys.items()):
-    print(f"{toy}: {count}")
+[print(f"{toy}: {crafted_toys.count(toy)}") for toy in sorted(set(crafted_toys))]
