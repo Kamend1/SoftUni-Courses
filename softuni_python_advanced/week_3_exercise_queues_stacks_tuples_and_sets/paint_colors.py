dd2d89ae406a72f9391f28d512f3bc0bd55d8d32
Kamend1 <142220912+Kamend1@users.noreply.github.com>
2024-01-17 17:00:11 +0200

    I have now started Advanced and OOP in January 2024. New exercise problems solved

--- softuni_python_advanced/week_3_exercise_queues_stacks_tuples_and_sets/paint_colors.py
+++ softuni_python_advanced/week_3_exercise_queues_stacks_tuples_and_sets/paint_colors.py
@@ -1,57 +1,38 @@
 from collections import deque
 
-main_colors = ['red', 'yellow', 'blue']
-secondary_colors = ["orange", "purple", "green"]
+colors = ['red', 'yellow', 'blue', "orange", "purple", "green"]
+
+req_colors = {
+    'orange': {'red', 'yellow'},
+    'purple': {'red', 'blue'},
+    'green': {'yellow', 'blue'},
+}
 
 substrings = deque(x for x in input().split())
 
 colors_found = []
 
 while substrings:
-    if len(substrings) == 1:
-        test_string = substrings[0]
-        test_string_1 = ""
+    test_string_1 = substrings.popleft()
+    test_string_2 = substrings.pop() if substrings else ''
+    combined_test_string = test_string_1 + test_string_2
+    reverse_test_string = test_string_2 + test_string_1
+
+    if combined_test_string in colors:
+        colors_found.append(combined_test_string)
+    elif reverse_test_string in colors:
+        colors_found.append(reverse_test_string)
     else:
-        test_string = substrings[0] + substrings[-1]
-        test_string_1 = substrings[-1] + substrings[0]
-    if test_string in main_colors or test_string in secondary_colors:
-        colors_found.append(test_string)
-        if len(substrings) >= 1:
-            substrings.popleft()
-        if len(substrings) >= 1:
-            substrings.pop()
-    elif test_string_1 in secondary_colors or test_string_1 in main_colors:
-        colors_found.append(test_string_1)
-        if len(substrings) >= 1:
-            substrings.popleft()
-        if len(substrings) >= 1:
-            substrings.pop()
-    else:
-        new_string_1 = substrings[0][:-1]
-        new_string_2 = substrings[-1][:-1]
-        if len(substrings) >= 1:
-            substrings.popleft()
-        if len(substrings) >= 1:
-            substrings.pop()
+        new_string_1 = test_string_1[:-1]
+        new_string_2 = test_string_2[:-1]
+
         if new_string_1:
             substrings.insert(len(substrings) // 2, new_string_1)
         if new_string_2:
             substrings.insert(len(substrings) // 2, new_string_2)
 
-if "orange" in colors_found:
-    if "red" not in colors_found  or "yellow" not in colors_found:
-        for color in colors_found:
-            if color == "orange":
-                colors_found.remove(color)
-elif "purple" in colors_found:
-    if "red" not in colors_found  or "blue" not in colors_found:
-        for color in colors_found:
-            if color == "purple":
-                colors_found.remove(color)
-elif "green" in colors_found:
-    if "yellow" not in colors_found  or "blue" not in colors_found:
-        for color in colors_found:
-            if color == "green":
-                colors_found.remove(color)
+for color in set(req_colors.keys()).intersection(colors_found):
+    if not req_colors.get(color).issubset(colors_found):
+        colors_found.remove(color)
 
 print(colors_found)
