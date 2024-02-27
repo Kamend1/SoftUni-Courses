c31b99ed1974610545065baed1ce5c6035de3758
Kamend1 <142220912+Kamend1@users.noreply.github.com>
2024-02-27 20:39:55 +0200

    I have now started Advanced and OOP in January 2024. New exercise problems solved

---
+++ softuni_python_OOP/week_3_inheritance/stack_of_strings.py
@@ -0,0 +1,27 @@
+class Stack:
+
+    def __init__(self):
+        self.data = []
+
+    def push(self, element: str):
+        self.data.append(element)
+
+    def pop(self):
+        return self.data.pop()
+
+    def top(self):
+        return self.data[-1]
+
+    def is_empty(self):
+        return True if self.data else False
+
+        # if len(self.data) == 0:
+        #     is_empty = True
+        # else:
+        #     is_empty = False
+        #
+        # return is_empty
+
+    def __str__(self):
+        result = ', '.join(reversed(self.data))
+        return f"[{result}]"
