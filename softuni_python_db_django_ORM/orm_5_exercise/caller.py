6fae4f07112947f6170936985556c5fcd0a900ae
Kamend1 <142220912+Kamend1@users.noreply.github.com>
2024-07-01 13:46:23 +0300

    Updated solutions and exercises

--- softuni_python_db_django_ORM/orm_5_exercise/caller.py
+++ softuni_python_db_django_ORM/orm_5_exercise/caller.py
@@ -1,10 +1,320 @@
 import os
+from typing import List
 import django
+from django.db.models import QuerySet
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models
+from main_app.models import ArtworkGallery, Laptop, BrandChoices, OsChoices, ChessPlayer, Meal, Dungeon, Workout
+
 # Create and check models
+
+
 # Run and print your queries
+def show_highest_rated_art() -> str:
+
+    highest_rated_art = ArtworkGallery.objects.order_by('-rating').first()
+
+    return f"{highest_rated_art.art_name} is the highest-rated art with a {highest_rated_art.rating} rating!"
+
+
+def bulk_create_arts(first_art: ArtworkGallery, second_art: ArtworkGallery) -> None:
+    new_arts = [first_art, second_art]
+
+    ArtworkGallery.objects.bulk_create(new_arts)
+
+
+def delete_negative_rated_arts() -> None:
+    ArtworkGallery.objects.filter(rating__lt=0).delete()
+
+
+def show_the_most_expensive_laptop() -> str:
+    most_expensive_laptop = Laptop.objects.order_by('-price', '-id').first()
+
+    return f"{most_expensive_laptop.brand} is the most expensive laptop available for {most_expensive_laptop.price}$!"
+
+
+def bulk_create_laptops(args: List[Laptop]) -> None:
+    Laptop.objects.bulk_create(args)
+
+
+def update_to_512_GB_storage() -> None:
+    Laptop.objects.filter(brand__in=[BrandChoices.ASUS, BrandChoices.LENOVO]).update(storage=512)
+
+
+def update_to_16_GB_memory() -> None:
+    Laptop.objects.filter(brand__in=[BrandChoices.APPLE, BrandChoices.DELL, BrandChoices.ACER]).update(memory=16)
+
+
+def update_operation_systems() -> None:
+    Laptop.objects.filter(brand=BrandChoices.ASUS).update(operation_system=OsChoices.WINDOWS)
+    Laptop.objects.filter(brand=BrandChoices.APPLE).update(operation_system=OsChoices.MACOS)
+    Laptop.objects.filter(brand__in=[BrandChoices.DELL, BrandChoices.ACER]).update(operation_system=OsChoices.LINUX)
+    Laptop.objects.filter(brand=BrandChoices.LENOVO).update(operation_system=OsChoices.CHROM_OS)
+
+
+def delete_inexpensive_laptops() -> None:
+    Laptop.objects.filter(price__lt=1200).delete()
+
+
+def bulk_create_chess_players(args: List[ChessPlayer]) -> None:
+    ChessPlayer.objects.bulk_create(args)
+
+
+def delete_chess_players() -> None:
+    ChessPlayer.objects.filter(title='no title').delete()
+
+
+def change_chess_games_won() -> None:
+    ChessPlayer.objects.filter(title="GM").update(games_won=30)
+
+
+def change_chess_games_lost() -> None:
+    ChessPlayer.objects.filter(title='no title').update(games_lost=25)
+
+
+def change_chess_games_drawn() -> None:
+    ChessPlayer.objects.update(games_drawn=10)
+
+
+def grand_chess_title_GM() -> None:
+    ChessPlayer.objects.filter(rating__gte=2400).update(title='GM')
+
+
+def grand_chess_title_IM() -> None:
+    ChessPlayer.objects.filter(rating__range=(2300, 2399)).update(title='IM')
+
+
+def grand_chess_title_FM() -> None:
+    ChessPlayer.objects.filter(rating__range=(2200, 2299)).update(title='FM')
+
+
+def grand_chess_title_regular_player() -> None:
+    ChessPlayer.objects.filter(rating__lte=2199).update(title='regular player')
+
+
+def set_new_chefs() -> None:
+    Meal.objects.filter(meal_type='Breakfast').update(chef='Gordon Ramsay')
+    Meal.objects.filter(meal_type='Lunch').update(chef='Julia Child')
+    Meal.objects.filter(meal_type='Dinner').update(chef='Jamie Oliver')
+    Meal.objects.filter(meal_type='Snack').update(chef='Thomas Keller')
+
+
+def set_new_preparation_times() -> None:
+    Meal.objects.filter(meal_type='Breakfast').update(preparation_time='10 minutes')
+    Meal.objects.filter(meal_type='Lunch').update(preparation_time='12 minutes')
+    Meal.objects.filter(meal_type='Dinner').update(preparation_time='15 minutes')
+    Meal.objects.filter(meal_type='Snack').update(preparation_time='5 minutes')
+
+
+def update_low_calorie_meals() -> None:
+    Meal.objects.filter(meal_type__in=["Breakfast" , "Dinner" ]).update(calories=400)
+
+
+def update_high_calorie_meals() -> None:
+    Meal.objects.filter(meal_type__in=["Lunch", "Snack"]).update(calories=700)
+
+
+def delete_lunch_and_snack_meals() -> None:
+    Meal.objects.filter(meal_type__in=["Lunch", "Snack"]).delete()
+
+
+def show_hard_dungeons() -> str:
+    hard_dungeons = Dungeon.objects.filter(difficulty='Hard').order_by('-location')
+    result = [f"{hd.name} is guarded by {hd.boss_name} who has {hd.boss_health} health points!" for hd in hard_dungeons]
+    return '\n'.join(result)
+
+
+def bulk_create_dungeons(args: List[Dungeon]) -> None:
+    Dungeon.objects.bulk_create(args)
+
+
+def update_dungeon_names() -> None:
+    Dungeon.objects.filter(difficulty='Easy').update(name='The Erased Thombs')
+    Dungeon.objects.filter(difficulty='Medium').update(name='The Coral Labyrinth')
+    Dungeon.objects.filter(difficulty='Hard').update(name='The Lost Haunt')
+
+
+def update_dungeon_bosses_health() -> None:
+    Dungeon.objects.exclude(difficulty='Easy').update(boss_health=500)
+
+
+def update_dungeon_recommended_levels() -> None:
+    Dungeon.objects.filter(difficulty='Easy').update(recommended_level=25)
+    Dungeon.objects.filter(difficulty='Medium').update(recommended_level=50)
+    Dungeon.objects.filter(difficulty='Hard').update(recommended_level=75)
+
+
+def update_dungeon_rewards() -> None:
+    Dungeon.objects.filter(boss_health=500).update(reward='1000 Gold')
+    Dungeon.objects.filter(location__startswith='E').update(reward='New dungeon unlocked')
+    Dungeon.objects.filter(location__endswith='s').update(reward='Dragonheart Amulet')
+
+
+def set_new_locations() -> None:
+    Dungeon.objects.filter(recommended_level=25).update(location='Enchanted Maze')
+    Dungeon.objects.filter(recommended_level=50).update(location='Grimstone Mines')
+    Dungeon.objects.filter(recommended_level=75).update(location='Shadowed Abyss')
+
+
+def show_workouts() -> str:
+    workouts = Workout.objects.filter(workout_type__in=["Calisthenics", "CrossFit"]).order_by('id')
+    result = [f"{w.name} from {w.workout_type} type has {w.difficulty} difficulty!" for w in workouts]
+    return '\n'.join(result)
+
+
+def get_high_difficulty_cardio_workouts() -> QuerySet:
+    return Workout.objects.filter(workout_type="Cardio", difficulty="High").order_by('instructor')
+
+
+def set_new_instructors() -> None:
+    Workout.objects.filter(workout_type="Cardio").update(instructor='John Smith')
+    Workout.objects.filter(workout_type="Strength").update(instructor='Michael Williams')
+    Workout.objects.filter(workout_type="Yoga").update(instructor='Emily Johnson')
+    Workout.objects.filter(workout_type="CrossFit").update(instructor='Sarah Davis')
+    Workout.objects.filter(workout_type="Calisthenics").update(instructor='Chris Heria')
+
+
+def set_new_duration_times() -> None:
+    Workout.objects.filter(instructor='John Smith').update(duration='15 minutes')
+    Workout.objects.filter(instructor='Sarah Davis').update(duration='30 minutes')
+    Workout.objects.filter(instructor='Chris Heria').update(duration='45 minutes')
+    Workout.objects.filter(instructor='Michael Williams').update(duration='1 hour')
+    Workout.objects.filter(instructor='Emily Johnson').update(duration='1 hour and 30 minutes')
+
+
+def delete_workouts() -> None:
+    Workout.objects.exclude(workout_type__in=["Strength", "Calisthenics"]).delete()
+
+# artwork1 = ArtworkGallery(artist_name='Vincent van Gogh', art_name='Starry Night', rating=4, price=1200000.0)
+# artwork2 = ArtworkGallery(artist_name='Leonardo da Vinci', art_name='Mona Lisa', rating=5, price=1500000.0)
+#
+# # Bulk saves the instances
+# # bulk_create_arts(artwork1, artwork2)
+# print(show_highest_rated_art())
+# print(ArtworkGallery.objects.all())
+# delete_negative_rated_arts()
+
+# laptop1 = Laptop(
+#     brand='Asus',
+#     processor='Intel Core i5',
+#     memory=8,
+#     storage=256,
+#     operation_system='MacOS',
+#     price=899.99
+# )
+# laptop2 = Laptop(
+#     brand='Apple',
+#     processor='Chrome OS',
+#     memory=16,
+#     storage=256,
+#     operation_system='MacOS',
+#     price=1399.99
+# )
+# laptop3 = Laptop(
+#     brand='Lenovo',
+#     processor='AMD Ryzen 7',
+#     memory=12,
+#     storage=256,
+#     operation_system='Linux',
+#     price=999.99,
+# )
+#
+# # Create a list of instances
+# laptops_to_create = [laptop1, laptop2, laptop3]
+#
+# # Use bulk_create to save the instances
+# bulk_create_laptops(laptops_to_create)
+#
+# update_to_512_GB_storage()
+# update_operation_systems()
+#
+# # Retrieve 2 laptops from the database
+# asus_laptop = Laptop.objects.filter(brand__exact='Asus').get()
+# lenovo_laptop = Laptop.objects.filter(brand__exact='Lenovo').get()
+#
+# print(asus_laptop.storage)
+# print(lenovo_laptop.operation_system)
+
+
+# print(show_the_most_expensive_laptop())
+
+# # Create two instances
+# dungeon1 = Dungeon(
+#     name="Dungeon 1",
+#     boss_name="Boss 1",
+#     boss_health=1000,
+#     recommended_level=75,
+#     reward="Gold",
+#     location="Eternal Hell",
+#     difficulty="Hard",
+# )
+#
+# dungeon2 = Dungeon(
+#     name="Dungeon 2",
+#     boss_name="Boss 2",
+#     boss_health=400,
+#     recommended_level=25,
+#     reward="Experience",
+#     location="Crystal Caverns",
+#     difficulty="Easy",
+# )
+
+# Bulk save the instances
+# bulk_create_dungeons([dungeon1, dungeon2])
+#
+# # Update boss's health
+# update_dungeon_bosses_health()
+
+# Show hard dungeons
+# hard_dungeons_info = show_hard_dungeons()
+# print(hard_dungeons_info)
+# #
+# # Change dungeon names based on difficulty
+# update_dungeon_names()
+# dungeons = Dungeon.objects.order_by('boss_health')
+# print(dungeons[0].name)
+# print(dungeons[1].name)
+#
+# # Change the dungeon rewards
+# update_dungeon_rewards()
+# dungeons = Dungeon.objects.order_by('boss_health')
+# print(dungeons[0].reward)
+# print(dungeons[1].reward)
+
+# Create two Workout instances
+# workout1 = Workout.objects.create(
+#     name="Push-Ups",
+#     workout_type="Calisthenics",
+#     duration="10 minutes",
+#     difficulty="Intermediate",
+#     calories_burned=200,
+#     instructor="Bob"
+# )
+#
+# workout2 = Workout.objects.create(
+#     name="Running",
+#     workout_type="Cardio",
+#     duration="30 minutes",
+#     difficulty="High",
+#     calories_burned=400,
+#     instructor="Lilly"
+# )
+#
+# # Run the functions
+# print(show_workouts())
+#
+# high_difficulty_cardio_workouts = get_high_difficulty_cardio_workouts()
+# for workout in high_difficulty_cardio_workouts:
+#     print(f"{workout.name} by {workout.instructor}")
+#
+# set_new_instructors()
+# for workout in Workout.objects.all():
+#     print(f"Instructor: {workout.instructor}")
+#
+# set_new_duration_times()
+# for workout in Workout.objects.all():
+#     print(f"Duration: {workout.duration}")
--- softuni_python_db_django_ORM/orm_5_exercise/main_app/models.py
+++ softuni_python_db_django_ORM/orm_5_exercise/main_app/models.py
@@ -60,3 +60,36 @@ class Workout(models.Model):
     difficulty = models.CharField(max_length=50)
     calories_burned = models.PositiveIntegerField()
     instructor = models.CharField(max_length=100)
+
+
+class ArtworkGallery(models.Model):
+    artist_name = models.CharField(max_length=100)
+    art_name = models.CharField(max_length=100)
+    rating = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class BrandChoices(models.TextChoices):
+    ASUS = "Asus", "Asus"
+    ACER = "Acer", "Acer"
+    APPLE = "Apple", "Apple"
+    LENOVO = "Lenovo", "Lenovo"
+    DELL = "Dell", "Dell"
+
+
+class OsChoices(models.TextChoices):
+    WINDOWS = "Windows", "Windows"
+    MACOS = "MacOS", 'MacOS'
+    LINUX = "Linux", 'Linux'
+    CHROM_OS = "Chrome OS", 'Chrome OS'
+
+
+class Laptop(models.Model):
+    brand = models.CharField(max_length=20, choices=BrandChoices.choices)
+    processor = models.CharField(max_length=100)
+    memory = models.PositiveIntegerField(help_text="Memory in GB")
+    storage = models.PositiveIntegerField(help_text="Storage in GB")
+    operation_system = models.CharField(max_length=10, choices=OsChoices.choices)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
